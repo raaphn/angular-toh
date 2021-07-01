10afe6fd046ee03e0189d921aea83df899d6c1bb
raaphn <raaph.n@gmail.com>
2021-07-01 20:36:16 -0300

    List Display

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Civic 1972'},
+    { id: 12, name: 'Civic 1979'},
+    { id: 13, name: 'Civic 1983'},
+    { id: 14, name: 'Civic 1987'},
+    { id: 15, name: 'Civic 1991'},
+    { id: 16, name: 'Civic 1995'},
+    { id: 17, name: 'Civic 2000'},
+    { id: 18, name: 'Civic 2005'},
+    { id: 19, name: 'Civic 2011'},
+    { id: 20, name: 'Civic 2015'}
+]
